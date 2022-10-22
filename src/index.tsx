1d155424c110d617ec168f218089961409452101
Emil Ahlberg <emil.ahlberg1@gmail.com>
2022-10-22 13:55:40 +0200

    eslint config

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 ReactDOM.render(
-<div>
-<h1>Hello, Welcome to React and TypeScript</h1>
-</div>,
+  <div>
+    <h1>Hello, Welcome to React and TypeScript</h1>
+  </div>,
   document.getElementById("root")
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require('path')
 module.exports = {
   entry: './src/index.tsx',
   module: {
@@ -6,15 +6,15 @@ module.exports = {
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
+        exclude: /node_modules/
+      }
+    ]
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.js']
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
+    path: path.resolve(__dirname, 'dist')
+  }
+}
