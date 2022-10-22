2dd3b6983f2d1b24920847e3b746947d225a2cf9
Emil Ahlberg <emil.ahlberg1@gmail.com>
2022-10-22 16:44:45 +0200

    working webpack serving react

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+const App = (
+    <div>
+        <h1>Hello, Welcome to React and TypeScript</h1>
+    </div>     
+)
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,47 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 const path = require('path')
 module.exports = {
-  entry: './src/index.tsx',
+  context: __dirname,
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: '/'
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json']
+  },
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
+        test: /\.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.ts$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.tsx$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|j?g|svg|gif)?$/,
+        use: 'file-loader'
       }
     ]
   },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js']
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+      filename: 'index.html'
+    })
+  ]
 }
